e0e8f3054289a25a485a40b34cdb6ef09f7c2490
mianak78 <rosebourn@gmail.com>
2020-02-17 19:43:25 -0700

    Updates

---
+++ precache-manifest.45732f4dae441897732eb4230e628aad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f45ab3608d847d55311e870c95e832a",
+    "url": "/react-employee-directory3/index.html"
+  },
+  {
+    "revision": "0e7b06951928775d8e2a",
+    "url": "/react-employee-directory3/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d3d2a6e408e623a33817",
+    "url": "/react-employee-directory3/static/js/2.10cd4938.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-employee-directory3/static/js/2.10cd4938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e7b06951928775d8e2a",
+    "url": "/react-employee-directory3/static/js/main.4f681338.chunk.js"
+  },
+  {
+    "revision": "f6f6ff11c6ec3760b28b",
+    "url": "/react-employee-directory3/static/js/runtime-main.34ad7c92.js"
+  }
+]);
--- src/utils/API.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export default{
-    getEmployees: function(limit) {
-        return axios.get(`https://randomuser.me/api/?results=${limit}&nat=us`)
-    }
-}
